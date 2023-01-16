0b10c52a1e01f6e90a41bed675e94faec1d782d3
Christoph Ladurner <christoph.ladurner@tugraz.at>
2023-01-16 14:57:31 +0100

    release v0.1.2

--- invenio_pure/__init__.py
+++ invenio_pure/__init__.py
@@ -11,7 +11,7 @@ from .errors import PureAPIError, PureRuntimeError
 from .ext import InvenioPure
 from .types import URL, PureConfigs, PureID, PureRecord
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 __all__ = (
     "__version__",
